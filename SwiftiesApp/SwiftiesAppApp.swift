31a766e40ad7b070eb67b8a00c8eaf8a7cf145e1
Mariana Martínez <mmarianaamy@gmail.com>
2024-10-08 17:17:48 -0600

    Initial Commit

---
+++ SwiftiesApp/SwiftiesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftiesAppApp.swift
+//  SwiftiesApp
+//
+//  Created by Alumno on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftiesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

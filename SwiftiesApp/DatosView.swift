e54398f76c48a14594fed6d94d5b761bedfb70b4
Mariana Martínez <mmarianaamy@gmail.com>
2024-10-09 15:16:38 -0600

    Start Dato Registros

--- SwiftiesApp/DatosView.swift
+++ SwiftiesApp/DatosView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct DatosView: View {
+    @State var selection = 0
+    let day = Calendar.current.component(.day, from: Date())
+    let month = Calendar.current.component(.month, from: Date())
+    
+    var monthDict = [1: "Enero",
+                            2: "Febrero",
+                            3: "Marzo",
+                            9: "Octubre"]
+
     var body: some View {
-        Text("hi")
+        Picker("What is your favorite color?", selection: $selection) {
+            Text("Hábitos").tag(0)
+            Text("Diario").tag(1)
+        }
+        .pickerStyle(.segmented).padding()
+        if (selection == 0){
+            Text("Hábitos").font(.largeTitle)
+        }else{
+            Text(String(day) + " " + (monthDict[month] ?? "Octubre")).font(.largeTitle)
+        }
     }
 }
 

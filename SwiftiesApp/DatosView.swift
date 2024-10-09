f6f6d31156f6fe8f415c45eac1e0121d98b0fba7
Mariana Martínez <mmarianaamy@gmail.com>
2024-10-09 14:49:01 -0600

    Start de tabs

---
+++ SwiftiesApp/DatosView.swift
@@ -0,0 +1,18 @@
+//
+//  DatosView.swift
+//  SwiftiesApp
+//
+//  Created by Alumno on 09/10/24.
+//
+
+import SwiftUI
+
+struct DatosView: View {
+    var body: some View {
+        Text("hi")
+    }
+}
+
+#Preview {
+    DatosView()
+}
---
+++ SwiftiesApp/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  SwiftiesApp
+//
+//  Created by Alumno on 09/10/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        TabView{
+            DatosView().tabItem{
+                Label("Datos", systemImage: "square.and.pencil")            }
+        }
+    }
+}
+
+#Preview {
+    MenuView()
+}
